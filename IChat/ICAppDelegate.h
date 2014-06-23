57d21043d2d7868c97a36888dda284ef15c144e3
chunlei <chunlei@chunleideMacBook-Pro.local>
2014-06-23 21:00:10 +0800

    Initial Commit

---
+++ IChat/ICAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ICAppDelegate.h
+//  IChat
+//
+//  Created by chunlei on 14-6-23.
+//  Copyright (c) 2014年 chunlei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ICAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ IChat/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  IChat
+//
+//  Created by chunlei on 14-6-23.
+//  Copyright (c) 2014年 chunlei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ICAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ICAppDelegate class]));
+    }
+}
